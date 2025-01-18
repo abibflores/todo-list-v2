317c5405b084e12c7b27ff2f95d009c31d72a342
abibflores <gon_abib@hotmail.com>
2025-01-18 05:25:31 -0600

    Refactor getAllTodos function for improved readability and consistency

--- src/services/todoList.ts
+++ src/services/todoList.ts
@@ -1,21 +1,16 @@
 import { Todo } from "@/interfaces/Todo";
 
 const API_URL = process.env.API_ROOT;
-const PATH = '/todo-list-banco-azteca-demo';
+const PATH = "/todo-list-banco-azteca-demo";
 
-export const getAllTodos = async(): Promise<Todo[]> => {
-    const response = await fetch(
-      `${API_URL}${PATH}`,
-      {
-        next: { revalidate: 10 },
-      }
-    );
-  
-    if (!response.ok) {
-      throw new Error('Error al obtener los To-Dos.');
-    }
-  
-    const data = await response.json();
-  
-    return data.body?.items || [];
-}
+export const getAllTodos = async (): Promise<Todo[]> => {
+  const response = await fetch(`${API_URL}${PATH}`);
+
+  if (!response.ok) {
+    throw new Error("Error al obtener los To-Dos.");
+  }
+
+  const data = await response.json();
+
+  return data.body?.items || [];
+};
